9c31f15141a632ccec1fbe36a56fb42f5ea0240b
Pe_te_r <mburupeter8526@gmail.com>
2024-04-10 18:07:42 +0300

    capture piece logic and pieceing moving logic

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 import pygame
 from Game import Game
 from Piece import Pawn
-from constant import BLACK_MARK,RED_MARK
-
+from constant import BLACK_MARK,RED_MARK,WHITE_MARK
+from copy import deepcopy
 
 class Main:
 
@@ -17,18 +17,17 @@ class Main:
     def mark_squares(self):
         if len(self.valid_moves_position) >0:
             for square in self.valid_moves_position:
-                # center_y = move[0]* 100 + 100 // 2
-                # center_x = move[1] * 100 + 100 // 2
                 center_x=100//2
                 center_y=100//2
-                radius = min(100 // 2 - 30, 40)  
-                pygame.draw.circle(square.surface, BLACK_MARK, (center_x, center_y), radius)
+                radius = min(100 // 2 - 30, 40)
+                if self.game.selected_piece.color =='black':
+                    pygame.draw.circle(square.surface, BLACK_MARK, (center_x, center_y), radius)
+                else:
+                    pygame.draw.circle(square.surface,WHITE_MARK,(center_x,center_y),radius)
                 square.show_square()
 
         if len(self.capture_postion)>0:
             for square in self.capture_postion:
-                # center_x=move[1]*100+100//2
-                # center_y=move[0]*100+100//2
                 center_x=100//2
                 center_y=100//2
                 radius=min(100//2-30,40)
@@ -38,57 +37,36 @@ class Main:
 
     def unmark_squares(self):
         for square in self.valid_moves_position:
-            # square=self.game.squares[pos[1]][pos[0]]
             square.declare_empty()
         
         for square in self.capture_postion:
-            # square=self.game.squares[pos[1]][pos[0]]
             piece=square.piece
             square.declare_empty()
             square.add_piece(piece)
         self.capture_postion=[]
     
-    def validate_moves(self,valid_moves):
-        valid_moves=valid_moves
-        print(valid_moves)
-        for position in valid_moves:
-            square_draw=self.game.squares[position[0]][position[1]]
-            if not square_draw.is_empty():
-                print(position)
 
-        return valid_moves
-    
     def handle_marking(self,x,y):
         self.game.selected_piece=self.game.selected_square.piece
-        valid_moves=self.game.selected_piece.valid_moves(x,y)
+        valid_moves=self.game.selected_piece.valid_moves(x,y,self.game.squares)
         valid_moves=[(y,x)for x,y in valid_moves if 0<= x<=7 and 0<=y<8]
-   
-        for position in valid_moves:
-            next_square_mark=self.game.squares[position[1]][position[0]]
+        print(valid_moves)
+        if len(valid_moves)>0:
+            for position in valid_moves:
+                next_square_mark=self.game.squares[position[1]][position[0]]
+                if next_square_mark.is_empty():
+                    self.valid_moves_position.append(next_square_mark)
+                else:
+                    value=next_square_mark.value
+                    # if (self.game.selected_square.value>0 and value <0)or(self.game.selected_square.value<0 and value >0):
+                    if (self.game.selected_piece.color != next_square_mark.piece.color):
+                        self.capture_postion.append(next_square_mark)
+        else:
+            self.clear_cache()
 
-
-
-            if next_square_mark.is_empty():
-                self.valid_moves_position.append(next_square_mark)
-            else:
-                value=next_square_mark.value
-                if (self.game.selected_square.value>0 and value <0)or(self.game.selected_square.value<0 and value >0):
-                   
-                    self.capture_postion.append(next_square_mark)
-            
-        
-       
-
-                
-                
-                
         if len(self.valid_moves_position)>0:
             self.game.piece_selected=True
-        
-
         self.mark_squares()
-            
-        
 
     def handle_movement(self,x,y):
         square_to=self.game.squares[x][y]
@@ -96,14 +74,12 @@ class Main:
         self.unmark_squares()
         square_to.add_piece(piece)
         self.game.selected_square.declare_empty()
-        # self.game.selected_square=None
         self.piece_selected=False
         self.game.selected_square=None
         self.game.selected_piece=None
         self.valid_moves_position=[]
         
     def clear_cache(self):
-        # self.game.selected_piece=None
         self.unmark_squares()
         self.game.selected_piece=None
         self.game.selected_square=None
@@ -119,12 +95,11 @@ class Main:
         if self.game.selected_square==None:
             self.game.selected_square=self.game.squares[x][y]
         
-        
         if self.game.piece_selected:
             if self.game.squares[x][y] in self.valid_moves_position:
                 self.handle_movement(x,y)
             elif self.game.squares[x][y].is_empty():
-                print('m')
+                # print('m')
                 self.clear_cache()
             elif self.game.squares[x][y] in self.capture_postion:
                 # print('m')
@@ -135,18 +110,14 @@ class Main:
                 if not self.game.squares[x][y].is_empty():
                     print('another')
                     self.unmark_squares()
+                    # self.clear_cache()
+                    self.capture_postion=[]
                     self.valid_moves_position=[]
                     self.game.selected_square=self.game.squares[x][y]
                     self.game.selected_piece=self.game.selected_square.piece
-                    # self.clear_cache()
-                
-        
-
         if self.game.selected_square != None:
-            if not self.game.selected_square.is_empty(): #and not self.game.piece_selected: 
+            if not self.game.selected_square.is_empty():
                 self.handle_marking(x,y)
-
-
 
     def main_loop(self):
         game=self.game
@@ -162,13 +133,8 @@ class Main:
                     pos=pygame.mouse.get_pos()
                     mouse_x=pos[0]//100
                     mouse_y=pos[1]//100
-
-                    # self.handle_click(mouse_x,mouse_y)
                     self.handle_click(mouse_x,mouse_y)
-                    # print(self.game.selected_piece)
-
-            
-
+                    print(self.game.selected_piece)
             pygame.display.flip()
 
 
--- Piece.py
+++ Piece.py
@@ -2,6 +2,8 @@ import os.path as path
 from pygame import image
 
 class Piece:
+
+    '''Please enter a Doc String...'''
     def __init__(self,color,name,value):
         self.color=color
         self.name=name
@@ -21,24 +23,52 @@ class Piece:
         self.full_path ='/home/phantom/coding/python-code/python game/chess-ai-game/assets/imgs-80px'
         self.image=image.load(path.join(self.full_path,f'{self.color}_{self.name}.png'))
 
-class Pawn(Piece):
-    def __init__(self,color,name='pawn',value=1):
-        super().__init__(color,name,value)
+# class Pawn(Piece):
+#     def __init__(self,color,name='pawn',value=1):
+#         super().__init__(color,name,value)
 
-    def valid_moves(self,row,col):
-        valid_squares=[]
-        if self.color=='white':
-            if col==1:
-                valid_squares=[(row,col+1),(row,col+2)]
-            else:
-                valid_squares=[(row,col+1)]
-            return valid_squares 
-        else:
-            if col==6:
-                valid_squares=[(row,col-1),(row,col-2)]
-            else:    
-                valid_squares=[(row,col-1)]
-            return valid_squares
+#     def valid_moves(self,row,col,squars):
+#         valid_squares=[]
+#         if self.color=='white':
+#             if col==1:
+#                 valid_squares=[(row,col+1),(row,col+2)]
+#             else:
+#                 valid_squares=[(row,col+1)]
+#             return valid_squares 
+#         else:
+#             if col==6:
+#                 valid_squares=[(row,col-1),(row,col-2)]
+#             else:    
+#                 valid_squares=[(row,col-1)]
+#             return valid_squares
+
+class Pawn(Piece):
+    def __init__(self, color, name='pawn', value=1):
+        super().__init__(color, name, value)
+
+    def valid_moves(self, row, col, squares):
+        valid_squares = []
+
+        # Define the direction of movement based on the pawn's color
+        direction = 1 if self.color == 'white' else -1
+
+        # Check if the square in front of the pawn is empty
+        if squares[row][col + direction].is_empty():
+            valid_squares.append((row, col + direction))
+
+            # Check for the initial double move
+            if (col == 1 and self.color == 'white') or (col == 6 and self.color == 'black'):
+                if squares[row][col + 2 * direction].is_empty():
+                    valid_squares.append((row, col + 2 * direction))
+
+        # Check for capturing diagonally
+        for dcol in [-1, 1]:
+            if 0 <= row + dcol < 8:
+                target_square = squares[row + dcol][col + direction]
+                if not target_square.is_empty() and target_square.piece.color != self.color:
+                    valid_squares.append((row + dcol, col + direction))
+
+        return valid_squares
 
         
 
@@ -47,7 +77,7 @@ class Knight(Piece):
     def __init__(self,color,name='knight',value=4):
         super().__init__(color,name,value)
 
-    def valid_moves(self,row,col):
+    def valid_moves(self,row,col,squares):
         valid_squares=[
             (row+1,col+2),
             (row+2,col+1),
@@ -61,73 +91,192 @@ class Knight(Piece):
         return valid_squares
 
 class Bishop(Piece):
-    def __init__(self,color,name='bishop',value=3):
-        super().__init__(color,name,value)
+    def __init__(self, color, name='bishop', value=3):
+        super().__init__(color, name, value)
 
-    def valid_moves(self,col,row):
-        valid_moves=[]
-        diagonals = [
-        (1, 1),  # Top-right diagonal (m=1, c=1)
-        (-1, 1),  # Top-left diagonal (m=-1, c=1)
-        (1, -1),  # Bottom-right diagonal (m=1, c=-1)
-        (-1, -1)  # Bottom-left diagonal (m=-1, c=-1)
-    ]
-
-        for dr, dc in diagonals:
-            r, c = row + dr, col + dc
+    def valid_moves(self, row,col, squares):
+        moves=[]
+        deltas = [(1, 1), (-1, 1), (1, -1), (-1, -1)]
+        for dr, dc in deltas:
+            r,c = row + dr, col + dc
             while 0 <= r < 8 and 0 <= c < 8:
-                valid_moves.append((c, r))
+                square = squares[r][c]
+                if square.is_empty() or square.piece.color != self.color:
+                    moves.append((r, c))
+                    if not square.is_empty():
+                        break
+                else:
+                    break
                 r += dr
                 c += dc
         
-
-        return valid_moves
+        return moves
 
 class Queen(Piece):
-    def __init__(self,color,name='queen',value=8):
-        super().__init__(color,name,value)
+    def __init__(self, color, name='queen', value=8):
+        super().__init__(color, name, value)
     
-    def valid_moves(self,row,col):
-        moves = []
+    def valid_moves(self, row, col, squares):
+        valid_moves = []
 
-    # Generate rook moves (horizontal and vertical)
-        for i in range(8):
-            if i != col:
-                moves.append((row, i))  # Horizontal moves
-            if i != row:
-                moves.append((i, col))  # Vertical moves
+        # Check moves in the same row
+        for i in range(1, 8):
+            if row + i < 8:
+                square = squares[row + i][col]
+                if square.is_empty():
+                    valid_moves.append((row + i, col))
+                elif square.piece.color != self.color:
+                    valid_moves.append((row + i, col))
+                    break
+                else:
+                    break
+            else:
+                break
+
+        for i in range(1, 8):
+            if row - i >= 0:
+                square = squares[row - i][col]
+                if square.is_empty():
+                    valid_moves.append((row - i, col))
+                elif square.piece.color != self.color:
+                    valid_moves.append((row - i, col))
+                    break
+                else:
+                    break
+            else:
+                break
+
+        # Check moves in the same column
+        for i in range(1, 8):
+            if col + i < 8:
+                square = squares[row][col + i]
+                if square.is_empty():
+                    valid_moves.append((row, col + i))
+                elif square.piece.color != self.color:
+                    valid_moves.append((row, col + i))
+                    break
+                else:
+                    break
+            else:
+                break
+
+        for i in range(1, 8):
+            if col - i >= 0:
+                square = squares[row][col - i]
+                if square.is_empty():
+                    valid_moves.append((row, col - i))
+                elif square.piece.color != self.color:
+                    valid_moves.append((row, col - i))
+                    break
+                else:
+                    break
+            else:
+                break
 
         # Generate bishop moves (diagonal)
         deltas = [(1, 1), (-1, 1), (1, -1), (-1, -1)]
         for dr, dc in deltas:
             r, c = row + dr, col + dc
             while 0 <= r < 8 and 0 <= c < 8:
-                moves.append((r, c))
+                square = squares[r][c]
+                if square.is_empty() or square.piece.color != self.color:
+                    valid_moves.append((r, c))
+                    if not square.is_empty():
+                        break
+                else:
+                    break
                 r += dr
                 c += dc
 
-        return moves
+        return valid_moves
 
 class Castle(Piece):
-    def __init__(self,color,name='rook',value=5):
-        super().__init__(color,name,value)
+    def __init__(self, color, name='rook', value=5):
+        super().__init__(color, name, value)
 
-    def valid_moves(self,row,col):
-        valid_moves=[]
-        for i in range(8):
-            valid_moves.append((row+i,col))
-            valid_moves.append((row,col+i))
-            valid_moves.append((row-i,col))
-            valid_moves.append((row,col-i))
-        
+    def valid_moves(self, row, col, squares):
+        valid_moves = []
+
+        # Check moves in the same row
+        for i in range(1, 8):
+            if row + i < 8:
+                square = squares[row + i][col]
+                if square.is_empty():
+                    valid_moves.append((row + i, col))
+                elif square.piece.color != self.color:
+                    valid_moves.append((row + i, col))
+                    break
+                else:
+                    break
+            else:
+                break
+
+        for i in range(1, 8):
+            if row - i >= 0:
+                square = squares[row - i][col]
+                if square.is_empty():
+                    valid_moves.append((row - i, col))
+                elif square.piece.color != self.color:
+                    valid_moves.append((row - i, col))
+                    break
+                else:
+                    break
+            else:
+                break
+
+        # Check moves in the same column
+        for i in range(1, 8):
+            if col + i < 8:
+                square = squares[row][col + i]
+                if square.is_empty():
+                    valid_moves.append((row, col + i))
+                elif square.piece.color != self.color:
+                    valid_moves.append((row, col + i))
+                    break
+                else:
+                    break
+            else:
+                break
+
+        for i in range(1, 8):
+            if col - i >= 0:
+                square = squares[row][col - i]
+                if square.is_empty():
+                    valid_moves.append((row, col - i))
+                elif square.piece.color != self.color:
+                    valid_moves.append((row, col - i))
+                    break
+                else:
+                    break
+            else:
+                break
+
         return valid_moves
+
 
 
 class King(Piece):
     def __init__(self,color,name='king',value=10):
         super().__init__(color,name,value)
 
-    def valid_moves(self,row,col):
+    def valid_moves(self,row,col,squares):
         valid_moves=[(row+1,col),(row-1,col),(row,col+1),(row,col-1),(row-1,col-1),(row+1,col+1),(row+1,col-1),(row-1,col+1)]
-        return valid_moves
-    
+        # opponent_king_positions = self.get_opponent_king_positions(row, col, squares)
+        filtered_moves = []
+        for move in valid_moves:
+            r, c = move
+            if 0 <= r < 8 and 0 <= c < 8:
+                check_square=squares[move[0]][move[1]]
+                if check_square.is_empty():
+                    filtered_moves.append(move)
+        return filtered_moves
+
+    def get_opponent_king_positions(self, row, col, squares):
+        opponent_king_positions = []
+        for i in range(max(0, row - 1), min(8, row + 2)):
+            for j in range(max(0, col - 1), min(8, col + 2)):
+                if not (i == row and j == col):  # Exclude the king's own square
+                    square = squares[i][j]
+                    if not square.is_empty() and square.piece.color != self.color and square.piece.name == 'king':
+                        opponent_king_positions.append((i, j))
+        return opponent_king_positions
